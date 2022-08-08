efeba594ada5efd40efd102c98ccb4eda195a17d
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-08-08 16:21:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+// collect input 
+// regEx pattern for email = /(\w{2})[\w.-]+@([\w.]+\w)/ 
+// showing only tw words 
+// replace with ***
+//  render to the Dom immdialty the button is clicked
+
+const searchUser = document.querySelector(".search_btn");
+const results = document.querySelector('#result')
+searchUser.addEventListener("click", function () {
+console.log('chizoba');
+ let inputValue = document.getElementById('email--input').value
+    const reg = /(\w{2})[\w.-]+@([\w.]+\w)/ ;
+ let showresult  = inputValue.replace(reg , '$1*******@$2')
+     results.innerHTML = showresult
+
+})
+
+
+
+
+
+
