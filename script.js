d3fa4da4fedab51251f8128ef9a0efa41aa33308
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2022-08-12 18:15:46 +0100

    added Readme.md

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 // collect input 
 // regEx pattern for email = /(\w{2})[\w.-]+@([\w.]+\w)/ 
-// showing only tw words 
+// showing only two words 
 // replace with ***
 //  render to the Dom immdialty the button is clicked
 
@@ -10,14 +10,10 @@ const results = document.querySelector('#result')
 searchUser.addEventListener("click", function () {
 console.log('chizoba');
  let inputValue = document.getElementById('email--input').value
-    const reg = /(\w{2})[\w.-]+@([\w.]+\w)/ ;
- let showresult  = inputValue.replace(reg , '$1*******@$2')
-     results.innerHTML = showresult
-
-})
-
-
-
-
+    const reg = /(\w{2})[\w.-]+@([\w.]+\w)/;
+ let showresult  = inputValue.replace(reg , '$1*******@$2');
+      results.innerHTML = showresult
+ 
+});
 
 
